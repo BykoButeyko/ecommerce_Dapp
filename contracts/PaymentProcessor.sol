650c66dbf3c69ed4e733e12fc91a9d066c4e38bb
Byko <bykocrypto@gmail.com>
2020-10-15 22:00:20 +0200

    🛸 fix OpenZeppelin import issue

---
+++ contracts/PaymentProcessor.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.2;
+
+import  '../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol' ;
+
+contract PaymentProcessor {
+    address public admin;
+    IERC20 public dai;
+
+}
+
+
